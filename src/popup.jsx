538945c459a2699288f8aaea04edb8eb0e8330a2
Tymchyk Maksym <reilag69@gmail.com>
2017-12-07 20:06:42 +0200

    Update

--- src/popup.jsx
+++ src/popup.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import {render} from 'react-dom';
 import PopupApplication from 'Popup/index';
-import 'Popup/Analytics';
 
 const onContentLoaded = () => {
     let ComponentElement = document.getElementById('popup-application');
